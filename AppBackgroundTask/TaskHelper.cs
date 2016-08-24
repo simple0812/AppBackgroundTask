69552f875947d84884ecaa4d04d4878af7e18a8b
zhanglei <lei.zhang@sunscell.com>
2016-08-24 13:20:08 +0800

    first commit

---
+++ AppBackgroundTask/TaskHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using BackgroundTasks;
+
+namespace AppBackgroundTask
+{
+    public static class TaskHelper
+    {
+        public static async Task<BackgroundTaskRegistration> RegisterBackgroundTask(Type taskEntryPoint,
+                                                                string taskName,
+                                                                IBackgroundTrigger trigger,
+                                                                IBackgroundCondition condition)
+        {
+            var status = await BackgroundExecutionManager.RequestAccessAsync();
+            if (status == BackgroundAccessStatus.Unspecified || status == BackgroundAccessStatus.Denied)
+            {
+                return null;
+            }
+
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+                if (cur.Value.Name == taskName)
+                {
+                    cur.Value.Unregister(true);
+                }
+            }
+
+            var builder = new BackgroundTaskBuilder
+            {
+                Name = taskName,
+                TaskEntryPoint = taskEntryPoint.FullName
+            };
+
+            builder.SetTrigger(trigger);
+
+            if (condition != null)
+            {
+                builder.AddCondition(condition);
+            }
+
+            BackgroundTaskRegistration task = builder.Register();
+
+            Debug.WriteLine($"Task {taskName} registered successfully.");
+
+            return task;
+        }
+
+        public static async Task RegisterBackgroundTask()
+        {
+            var task = await RegisterBackgroundTask(
+                typeof(SayFarkTask),
+                "SayFarkTask",
+                new TimeTrigger(15, false),
+                null);
+
+            task.Progress += Task_Progress;
+            task.Completed += Task_Completed; ;
+        }
+
+        private static void Task_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            Debug.Write("........Task_Completed" + args.InstanceId);
+        }
+
+        private static void Task_Progress(BackgroundTaskRegistration sender, BackgroundTaskProgressEventArgs args)
+        {
+            Debug.Write("........Task_Progress" + args.InstanceId);
+        }
+    }
+}
---
+++ BackgroundTasks/SayFarkTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundTasks
+{
+    public sealed class SayFarkTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debug.Write("================ Fark the farking farkers ================");
+        }
+    }
+}
